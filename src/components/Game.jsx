8486348bcbb0c52799deecff1774658239fac2e0
Vinay Patil <vinfreecs>
2023-11-16 19:00:07 +0530

    Tasklist 3 almost completed

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,8 +7,14 @@ import oLogo from "../assets/o.svg";
 
 export default function Game() {
   const [openGame, setOpenGame] = useState(false);
+  const [winner, setWinner] = useState("");
   const [playerSelection, setPlayerSelection] = useState("");
   const [currPlayer, setCurrPlayer] = useState("");
+  const [status, setStatus] = useState({
+    user: 0,
+    ties: 0,
+    comp: 0,
+  });
   const [optionBtns, setOptionBtns] = useState([
     null,
     null,
@@ -21,41 +27,9 @@ export default function Game() {
     null,
   ]);
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (currPlayer != "" && currPlayer != playerSelection) {
-        const randomNumber = [];
-        for (let i = 0; i < optionBtns.length; i++) {
-          if (optionBtns[i] === null) randomNumber.push(i);
-        }
-        setOptionBtns((prevOptionBtns) => {
-          const newArr = [...prevOptionBtns]; // Create a copy of the previous state
-          newArr[
-            randomNumber[Math.floor(Math.random() * randomNumber.length)]
-          ] = currPlayer;
-          currPlayer === "x-btn"
-            ? setCurrPlayer("o-btn")
-            : setCurrPlayer("x-btn");
-          return newArr; // Return the updated state
-        });
-      }
-    }, 2000);
-  }, [currPlayer]);
-
-  const handleOptionBtn = (e) => {
-    if (
-      currPlayer === playerSelection &&
-      optionBtns[parseInt(e.target.id)] === null
-    ) {
-      setOptionBtns((prevOptionBtns) => {
-        const newArr = [...prevOptionBtns]; // Create a copy of the previous state
-        newArr[parseInt(e.target.id)] = currPlayer;
-        currPlayer === "x-btn"
-          ? setCurrPlayer("o-btn")
-          : setCurrPlayer("x-btn");
-        return newArr; // Return the updated state
-      });
-    }
+  const replayGame = () => {
+    setWinner("");
+    setOptionBtns([null, null, null, null, null, null, null, null, null]);
   };
 
   const checkWin = () => {
@@ -69,18 +43,140 @@ export default function Game() {
       [2, 4, 6],
       [6, 7, 8],
     ];
-    const currPlayerPos = []
-    for(let i=0;i<optionBtns.length;i++){
-      if(optionBtns[i]===currPlayer) currPlayerPos.push(i)
+    const currPlayerPos = [];
+    for (let i = 0; i < optionBtns.length; i++) {
+      if (optionBtns[i] === currPlayer) currPlayerPos.push(i);
     }
     winPatterns.forEach((a) => {
       if (a.every((ele) => currPlayerPos.includes(ele))) {
-        let winner = currPlayer;
-        console.log(winner+"won the game")
+        setWinner(currPlayer);
+        replayGame();
+        return true;
       }
     });
+    if (optionBtns.every((ele) => ele != null)) {
+      setWinner("ties");
+    }
+    return;
   };
-  checkWin();
+
+  const pickIndex = (compPlayer) => {
+    const winPatterns = [
+      [0, 1, 2],
+      [0, 3, 6],
+      [3, 4, 5],
+      [0, 4, 8],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+      [6, 7, 8],
+    ];
+    const userPlayer = [];
+    for (let i = 0; i < optionBtns.length; i++) {
+      if (optionBtns[i] === playerSelection) {
+        userPlayer.push(i);
+      }
+    }
+    if (userPlayer.length === 1) {
+      return 0;
+    }
+    let ans = [];
+    let inde = 0;
+    winPatterns.forEach((a) => {
+      let len = 0;
+      let ele = 0;
+      a.forEach((i) => {
+        if (userPlayer.includes(i)) len++;
+        else ele = i;
+      });
+      if (len == 2) {
+        ans.push(ele);
+        console.log(ele, ans);
+      }
+    });
+    ans.forEach((item) => {
+      if (compPlayer.includes(item)) {
+        inde = compPlayer.indexOf(item);
+        return;
+      }
+      if (inde != 0) return;
+    });
+    return inde;
+  };
+  const compTurn = () => {
+    setTimeout(() => {
+      if (currPlayer != "" && currPlayer != playerSelection) {
+        const randomNumberFirst = [4, 0, 2, 6, 8, 1, 3, 5, 7];
+        for (let i = 0; i < optionBtns.length; i++) {
+          if (optionBtns[i] != null) {
+            const ind = randomNumberFirst.indexOf(i);
+            if (ind > -1) {
+              randomNumberFirst.splice(ind, 1);
+            }
+          }
+        }
+        console.log(randomNumberFirst);
+        setOptionBtns((prevOptionBtns) => {
+          const newArr = [...prevOptionBtns]; // Create a copy of the previous state
+          newArr[randomNumberFirst[pickIndex(randomNumberFirst)]] = currPlayer;
+          currPlayer === "x-btn"
+            ? setCurrPlayer("o-btn")
+            : setCurrPlayer("x-btn");
+          return newArr; // Return the updated state
+        });
+      }
+    }, 0);
+  };
+
+  useEffect(() => {
+    let ans = checkWin();
+    compTurn();
+    console.log(winner, ans);
+    if (winner != "") {
+      console.log("entering", status);
+      setStatus({ ...status, ties: status.ties++ });
+      console.log(status)
+      replayGame();
+    }
+    // if(winner === ""){
+    //   console.log("testing")
+    // }else if(winner === playerSelection){
+    //   setStatus((prev) =>{
+    //     let newScore = {...prev}
+    //     console.log(newScore)
+    //     newScore.user = prev.user+1;
+    //     replayGame();
+    //   })
+    // }else if(winner === "tie"){
+    //   setStatus((prev) =>{
+    //     let newScore = {...prev}
+    //     newScore.ties = prev.ties+1;
+    //     replayGame();
+    //   })
+    // }else if(winner != "tie" && winner != playerSelection){
+    //   setStatus((prev) =>{
+    //     let newScore = {...prev}
+    //     newScore.comp = prev.comp+1;
+    //     replayGame();
+    //   })
+    // }
+  }, [optionBtns]);
+
+  const handleOptionBtn = (e) => {
+    if (
+      currPlayer === playerSelection &&
+      optionBtns[parseInt(e.target.id)] === null &&
+      winner === ""
+    ) {
+      setOptionBtns((prevOptionBtns) => {
+        const newArr = [...prevOptionBtns]; // Create a copy of the previous state
+        newArr[parseInt(e.target.id)] = currPlayer;
+        return newArr; // Return the updated state
+      });
+      currPlayer === "x-btn" ? setCurrPlayer("o-btn") : setCurrPlayer("x-btn");
+    }
+  };
+
   function handleSelectionClick(e) {
     console.log(e.target.id);
     setPlayerSelection(e.target.id);
@@ -99,6 +195,7 @@ export default function Game() {
           handleOptionBtn={handleOptionBtn}
           currPlayer={currPlayer}
           optionBtns={optionBtns}
+          status={status}
         />
       ) : (
         <Game_Home
--- src/components/Game_Place.jsx
+++ src/components/Game_Place.jsx
@@ -3,8 +3,13 @@ import xLogo from "../assets/x.svg";
 import oLogo from "../assets/o.svg";
 import { useState } from "react";
 
-export default function Game_Place({playerSelection,handleOptionBtn,currPlayer,optionBtns}) {
-    
+export default function Game_Place({
+  playerSelection,
+  handleOptionBtn,
+  currPlayer,
+  optionBtns,
+  status,
+}) {
   return (
     <>
       <div className="top-game-place">
@@ -13,18 +18,41 @@ export default function Game_Place({playerSelection,handleOptionBtn,currPlayer,o
           <img src={oLogo} alt="O" />
         </div>
         <div className="turn-display">
-            <img src={currPlayer==="x-btn" ? xLogo : oLogo} alt="" />
-            <p>TURN</p>
+          <img src={currPlayer === "x-btn" ? xLogo : oLogo} alt="" />
+          <p>TURN</p>
         </div>
       </div>
       <div className="threexthree">
-        {
-            optionBtns.map((btn,index)=>(
-                <button key={index} id={index} className="option-btn" onClick={handleOptionBtn}>
-                    {btn &&<img src={btn === "x-btn" ? xLogo :oLogo} alt="" />}
-                </button>
-            ))
-        }
+        {optionBtns.map((btn, index) => (
+          <button
+            key={index}
+            id={index}
+            className="option-btn"
+            onClick={handleOptionBtn}
+          >
+            {btn && <img src={btn === "x-btn" ? xLogo : oLogo} alt="" />}
+          </button>
+        ))}
+      </div>
+      <div className="game-scores-container">
+        <div className="player-score">
+          <p>
+          {playerSelection === "x-btn" ? <>X</> : <>O</>}
+            (YOU)
+          </p>
+          <p>{status.user}</p>
+        </div>
+        <div className="tie-score">
+          <p>TIES</p>
+          <p>{status.ties}</p>
+        </div>
+        <div className="comp-score">
+          <p>
+          {playerSelection === "x-btn" ? <>O</>: <>X</>} 
+            (CPU)
+          </p>
+          <p>{status.comp}</p>
+        </div>
       </div>
     </>
   );
